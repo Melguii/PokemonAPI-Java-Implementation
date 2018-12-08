4d976d7090562a2db72b8b9745eb50297df2477f
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-12-08 18:18:37 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,15 @@
+import com.Pokemon.Ball;
 import com.Pokemon.Menu;
 import com.google.gson.Gson;
 
 import java.io.FileReader;
 
 public class Main {
-    public static void Main (String[] args){
-        Gson gson = new Gson();
-
+    public static void main (String[] args) throws FileNotFoundException{
+        Gson gson = new GsonBuilder().create();
+        Ball[] pokeBalls = gson.fromJson(new FileReader("files/balls.json"),Ball[].class);
+        Ball[] pokeBalls = gson.fromJson(new FileReader("files/balls.json"),Ball[].class);
         Menu m = new Menu();
         m.menu();
-
-        //FileReader f = m.menu();
-        //User[] users = gson.fromJson(f, User[].class);
     }
 }
