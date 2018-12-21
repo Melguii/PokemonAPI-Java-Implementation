5e71d5534f2b4720aefcc44850feaad9328accf0
Gerard Melgares <gerard.melgares@students.salle.url.edu>
2018-12-21 18:26:58 +0100

    commented: pokeball

--- src/main/java/Jugador/Tienda.java
+++ src/main/java/Jugador/Tienda.java
@@ -8,17 +8,24 @@ public class Tienda {
     private Pokeball[] pokeballs;
 
     /**
-     *
-     * @return
+     *Getter del array de pokeballs
+     * @return Devuelve el array de pokeballs
      */
     public Pokeball[] getPokeballs() {
         return pokeballs;
     }
 
+    /**
+     * Setter de el array de pokeballs
+     * @param pokeballs registra un array de pokeballs
+     */
     public void setPokeballs(Pokeball[] pokeballs) {
         this.pokeballs = pokeballs;
     }
 
+    /**
+     * Mostramos todos los objetos de la tienda con sus precios correspondientes
+     */
     public void mostrarObjetos() {
         char i = 'a';
         System.out.println("Pokéballs disponibles:");
@@ -29,6 +36,11 @@ public class Tienda {
         System.out.println("\n"+ i + ") " + "Sortir sense comprar\n");
     }
 
+    /**
+     * Devolvemos tantos objetos del objeto que quiere el usuario
+     * @param eleccion el nombre del objeto
+     * @return la lista de todos los objetos (pokeball) que quiere el usuario
+     */
     public List<Pokeball> getObjetos(char eleccion) {
         List<Pokeball> pokeballs = new ArrayList<Pokeball>();
         int index = eleccion - 'A';
@@ -52,6 +64,10 @@ public class Tienda {
         }
     }
 
+    /**
+     *???????????????????????????
+     * @return
+     */
     public Pokeball getFirstPokeball() {
         return pokeballs[0];
     }
