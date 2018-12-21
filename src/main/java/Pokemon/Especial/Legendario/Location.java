0309e2d85f830f289ea8d6528f99b3d463c0537f
Gerard Melgares <gerard.melgares@students.salle.url.edu>
2018-12-21 18:37:31 +0100

    commented: location

--- src/main/java/Pokemon/Especial/Legendario/Location.java
+++ src/main/java/Pokemon/Especial/Legendario/Location.java
@@ -5,18 +5,34 @@ public class Location {
     private float longitude;
     private float latitude;
 
+    /**
+     * Getter de longitude
+     * @return Devuelve la longitude de location
+     */
     public float getLongitude() {
         return longitude;
     }
 
+    /**
+     * Setter de longitud
+     * @param longitude Inserta la longitude en location
+     */
     public void setLongitude(float longitude) {
         this.longitude = longitude;
     }
 
+    /**
+     * Getter de latitude
+     * @return Devuelve la latitude de location
+     */
     public float getLatitude() {
         return latitude;
     }
 
+    /**
+     * Setter de latitude
+     * @param latitude Inserta la latitude en location
+     */
     public void setLatitude(float latitude) {
         this.latitude = latitude;
     }
