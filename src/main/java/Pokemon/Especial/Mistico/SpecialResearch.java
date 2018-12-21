76e0e4f1bc0533ac65d742797c833ba0d5751127
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-12-21 18:14:14 +0100

    especialResearh

--- src/main/java/Pokemon/Especial/Mistico/SpecialResearch.java
+++ src/main/java/Pokemon/Especial/Mistico/SpecialResearch.java
@@ -5,30 +5,50 @@ import java.util.ArrayList;
 public class SpecialResearch {
 
     private String name;
+    private ArrayList<Quest> quests;
 
+
+    /**
+     * Devuelve las quest del pokemon
+     * @return quests
+     */
     public ArrayList<Quest> getQuests() {
         return quests;
     }
-
-    private ArrayList<Quest> quests;
 
     public SpecialResearch() {
         this.quests = new ArrayList<Quest>();
     }
 
+    /**
+     * Devuelve el nombre
+     * @return String name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Pone el nombre a la SpecialResearch
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-
+    /**
+     * Hace set de las quests
+     * @param quests
+     */
     public void setQuests(ArrayList<Quest> quests) {
         this.quests = quests;
     }
 
+    /**
+     * Comprueba que una quest esta cumplida o no, si esta cumplida lo actualiza
+     * @param id
+     * @return el porcentaje total de la SepecialResearch
+     */
     public int checkSpecialResearch(int id) {
         int sum = 0;
         for (Quest q: quests) {
@@ -40,6 +60,9 @@ public class SpecialResearch {
         return sum/quests.size();
     }
 
+    /**
+     * Resetea la research
+     */
     public void reset() {
         for (Quest q: quests) {
             q.reset();
